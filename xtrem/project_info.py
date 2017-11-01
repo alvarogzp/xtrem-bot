1862cee963da7a3ac826440a0a2b2da7dca75991
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-01 09:10:58 +0100

    Add project_info

---
+++ xtrem/project_info.py
@@ -0,0 +1,13 @@
+# Shared project info
+
+name = 'xtrem-bot'
+
+description = 'XtremBot Python Telegram bot'
+
+source_url = 'https://github.com/alvarogzp/xtrem-bot'
+
+author_name = 'Alvaro Gutierrez Perez'
+author_email = 'alvarogzp@gmail.com'
+author_handle = "@AlvaroGP"
+
+license_name = 'GNU AGPL 3.0'
