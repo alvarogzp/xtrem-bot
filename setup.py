4b8fd9cbff9bcfd2c810f90909fe6b1243bc50e1
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-11-01 09:11:35 +0100

    Add setup.py using xtrembot project_info and defining telegram-bot-framework as dependency

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+from xtrem import project_info
+
+setup(
+    name=project_info.name,
+
+    use_scm_version=True,
+
+    description=project_info.description,
+
+    url=project_info.source_url,
+
+    author=project_info.author_name,
+    author_email=project_info.author_email,
+
+    license=project_info.license_name,
+
+    packages=find_packages(),
+
+    setup_requires=[
+        'setuptools_scm'
+    ],
+
+    install_requires=[
+        'telegram-bot-framework'
+    ],
+
+    python_requires='>=3',
+)
